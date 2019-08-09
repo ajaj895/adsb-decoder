73698e65a833d82dcbf738edf54135e44bc3f531
Evan <41348743+ajaj895@users.noreply.github.com>
2019-08-09 14:20:47 -0500

    added experimental notification

---
+++ adsb/util/ExperimentalNotification.java
@@ -0,0 +1,16 @@
+package ads.b.decoder;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class ExperimentalNotification(){
+	public static void startPop(){
+
+		Stage window = new Stage();
+
+		window.initModality
+	}
+}
