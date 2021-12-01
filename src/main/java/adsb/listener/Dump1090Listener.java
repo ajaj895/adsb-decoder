0c03ccd4469516b7fa82d559004b9958b3543257
Evan C <eccolwell99@gmail.com>
2021-11-30 18:01:03 -0600

    Added WIP Dump1090Listener

---
+++ src/main/java/adsb/listener/Dump1090Listener.java
@@ -0,0 +1,36 @@
+/*
+ * Created on: Nov 30, 2021
+ * Author: Evan Colwell
+ *
+ * Description: This is a port listening class that will get the datastream from Dump1090.
+ *
+ * I will be using port 30002 for this class, it will get data in this format:
+ * *8D451E8B99019699C00B0A81F36E;
+ * It will end with a new line character. This information comes in as fast as 1090 gets it.
+ *
+ * More information about Dump1090 can be found here: https://github.com/antirez/dump1090
+ */
+
+package adsb.listener;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket; //Needed for listening to Dump1090 ports
+
+public class Dump1090Listener {
+
+    final int portnum = 30002;
+
+    private ServerSocket serverSock;
+    private PrintWriter printWriter;
+    private BufferedInputStream buffInStream;
+
+
+    public Dump1090Listener() throws IOException {
+
+        serverSock = new ServerSocket(portnum);
+
+
+    }
+}
